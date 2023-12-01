bd40012feefb45432b63889ce4e100fb9d52d62c
me-no <100861251+me-no@users.noreply.github.com>
2023-12-01 20:33:55 +0900

    add pwa manifest

---
+++ asagi/sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'asagi';
+var urlsToCache = [
+    './p5.js',
+    './p5.sound.min.js',
+    './sketch.js',
+    './style.css',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
